058797f2b0415c06fa403133f6c40b1d1abb1802
Rahul-pinjarla <rahulpinjarla@gmail.com>
2021-05-10 01:08:55 +0530

    release 1

--- cse/settings.py
+++ cse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#1avy6*qe70%fxa!7xzf3xc_phc9^b(lccgg#x=y9_+*7^p2^s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', '192.168.67.158']
+ALLOWED_HOSTS = ['*', 'arm-cse.herokuapp.com']
 
 
 # Application definition
